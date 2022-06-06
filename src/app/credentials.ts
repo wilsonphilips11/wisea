9e02118396e1d9971eab7bb3f57dd91d9dd39482
wilsonphilips11 <wilsonphilips11@gmail.com>
2022-06-07 01:55:38 +0700

    remove project

--- src/app/credentials.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: 'AIzaSyBSLMbliZdidFaCBfWuXkt2XM3SwBngQxg',
-    authDomain: 'wisea-f81b0.firebaseapp.com',
-    databaseURL: 'https://wisea-f81b0.firebaseio.com',
-    projectId: 'wisea-f81b0',
-    storageBucket: 'wisea-f81b0.appspot.com',
-    messagingSenderId: '318389370572'
-  };
--- src/app/model/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface UserData {
-  uid: string;
-  email: string;
-  username: string;
-  name: string;
-  photo: string;
-  phone: string;
-  balance: number;
-}
